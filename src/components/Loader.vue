ab2f240ccaa91ea5ef63ae8f325bdedffd0c58cc
AlessioBottin <alessiobottin2002@gmail.com>
2022-01-03 16:59:22 +0100

    loader is working

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -14,10 +14,21 @@ export default {
 .loader {
     text-align: center;
     margin-top: 200px;
+    animation: spin 2s infinite linear;
 
     .icon {
         color: white;
         font-size: 80px;
+        
+    }
+}
+
+@keyframes spin {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(359deg);
     }
 }
 </style>
