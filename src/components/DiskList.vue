813d8eb5276489d198fd4ad022f4d438f04abb34
AlessioBottin <alessiobottin2002@gmail.com>
2022-01-11 10:11:01 +0100

    done

--- src/components/DiskList.vue
+++ src/components/DiskList.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="disk-list">
         <div class="container">
-            <div v-if="disks.length > 0" class="disk-container">
-                <DiskCard v-for="(disk, index) in disks" :key="index" :diskObject="disk" />               
+
+            <Search @filter="setFilter"/>
+
+            <div v-if="!isApiLoading" class="disk-container">
+                <DiskCard v-for="(disk, index) in filterDisk" :key="index" :diskObject="disk" />               
             </div>
 
             <Loader v-else />
@@ -12,6 +15,7 @@
 
 <script>
 import DiskCard from "./DiskCard";
+import Search from "./Search";
 import Loader from "./Loader";
 import axios from 'axios';
 
@@ -20,16 +24,42 @@ export default {
     components: {
         DiskCard,
         Loader,
+        Search,
     },
     data: function() {
         return {
             disks: [],
+            isApiLoading: true,
+            genreFilter: 'all',
         };
+    },
+    methods: {
+        setFilter: function(genreText) {
+            this.genreFilter = genreText
+        }
+    },
+    computed: {
+        filterDisk: function(genreText) {
+            if (this.genreFilter === 'all') {
+                return this.disks
+            }
+
+            const FilteredDisks = this.disks.filter((singleDisk) => {
+                console.log(singleDisk.genre.toLowerCase());
+                console.log(this.genreFilter);
+
+                return singleDisk.genre.toLowerCase() === this.genreFilter;
+            });
+            
+            return FilteredDisks;
+
+        },
     },
     created: function() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((response) => {
             this.disks = response.data.response;
+            this.isApiLoading = false;
         });
     },
     
@@ -37,11 +67,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables';
+
 .disk-list {
 
     .container {
         padding: 50px 0;
 
+        // Disk 
         .disk-container {
             display: flex;
             justify-content: space-between;
---
+++ src/components/Search.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="search">
+        <div class="filter">
+                <select @change="$emit('filter', genreFilter)" v-model="genreFilter" name="genre" id="">
+                    <option value="all">All</option>
+                    <option value="rock">Rock</option>
+                    <option value="pop">Pop</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="metal">Metal</option>
+                </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    data: function() {
+        return {
+            genreFilter: 'all',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables';
+
+// Select 
+.filter {
+    margin-bottom: 30px;
+
+    select {
+        color: white;
+        background-color: $secondary_color;
+        border-color: white;
+        padding: 10px 5px;
+        font-size: 18px;
+        border-radius: 10px;
+    }
+} 
+</style>
